e571b03d0a1572f63afaabef37503670e7b5eb73
emiyl <me@emiyl.com>
2022-02-04 10:19:26 +0000

    remove get-started pages

--- docs/.vuepress/plugins/dynamicPages/lib/index.js
+++ docs/.vuepress/plugins/dynamicPages/lib/index.js
@@ -19,36 +19,11 @@ module.exports = function(themeLocale, localePath) {
     global.themeLocale = themeLocale[localePath[i]];
     
     var pageList = require('./deviceChart')
-    
-    function getPkgManPages(app) {
-      var ret = [];
-      for (const p in app.pages) {
-        const page = app.pages[p];
-        const fm = page.frontmatter
-        if (!fm.hasOwnProperty('pkgman')) continue;
-        ret.push({
-          path: page.path.replace('index.html', '').replace('.html','\/') + `using-${fm.pkgman}.html`,
-          frontmatter: {
-            lang: 'en_US',
-            title: `Using ${fm.pkgman.replace(/^\w/, c => c.toUpperCase())}`,
-            description: `Guide to using ${fm.pkgman.replace(/^\w/, c => c.toUpperCase())}`,
-            editLink: false,
-            lastUpdated: false,
-            contributors: false,
-          },
-          content: `!!!include(./docs/en_US/include/using-${fm.pkgman}.md)!!!`
-        })
-      }
-      return ret;
-    }
 
     return {
       name: 'vuepress-dynamic-pages',
       async onInitialized(app) {
         for (const p in pageList) app.pages.push(await createPage(app, pageList[p]))
-        
-        const pkgManPages = getPkgManPages(app);
-        for (const p in pkgManPages) app.pages.push(await createPage(app, pkgManPages[p]))
       }
     }
   }
