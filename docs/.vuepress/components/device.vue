a66798cb2dfaccb4772631145ff2d5fbbc0b76b8
emiyl <me@emiyl.com>
2022-03-03 14:07:51 +0000

    better mobile tables (horizontal scrolling)

--- docs/.vuepress/components/device.vue
+++ docs/.vuepress/components/device.vue
@@ -77,42 +77,44 @@
       </div>
     </li>
   </ul>
-  <table>
-    <tr v-if="simpleTable">
-      <th v-html="fromStr"/>
-      <th v-html="toStr"/>
-      <th v-html="jailbreakStr"/>
-    </tr>
-    <tr v-else>
-      <th v-html="buildStr" v-if="showBuildNum"/>
-      <th v-html="versionStr" v-if="showVersion"/>
-      <th v-html="jailbreakStr" v-if="showJailbreak"/>
-      <th v-html="releaseDateStr" v-if="showReleaseDate" style="width: 15%;"/>
-    </tr>
-    <template v-for="(fw, index) in fwArr" :key="fw">
+  <div class="tableContainer">
+    <table>
       <tr v-if="simpleTable">
-        <td v-for="i in (reverseSorting) ? Object.keys(fw).reverse() : Object.keys(fw)" :key="i">{{ fw[i].version }}</td>
-        <td v-if="fw.startBuild.jailbreakArr.length"><router-link :to="jailbreakPath + fw.startBuild.jailbreakArr[0].name.replace(/ /g, '-') + '.html'">{{ fw.startBuild.jailbreakArr[0].name }}</router-link></td>
-        <td v-else v-html="noJbStr"/>
+        <th v-html="fromStr"/>
+        <th v-html="toStr"/>
+        <th v-html="jailbreakStr"/>
       </tr>
       <tr v-else>
-        <td v-if="showBuildNum"><a :href="firmwarePath + fw.uniqueBuild + '.html'">{{fw.build}}</a></td>
-
-        <template v-if="showVersion">
-          <td v-if="!showBuildNum"><a :href="firmwarePath + fw.uniqueBuild + '.html'">{{fw.osStr}} {{fw.version}} <span v-if="fwArr.filter(x => x.version == fw.version && x.osStr == fw.osStr).length > 1">({{fw.build}})</span></a></td>
-          <td v-else>{{fw.osStr}} {{fw.version}}</td>
-        </template>
-        
-        <template v-if="showJailbreak">
-          <td v-if="fw.jailbreakArr.length > 0"><span v-for="(jb, index) in fw.jailbreakArr" :key="jb"><a :href="jailbreakPath + jb.name.replace(/ /g, '-') + '.html'" v-html="jb.name"/><span v-if="index+1 < fw.jailbreakArr.length">, </span></span></td>
-          <td v-else v-html="noJbStr"/>
-        </template>
-        
-        <td v-if="showReleaseDate">{{fw.released}}</td>
+        <th v-html="buildStr" v-if="showBuildNum"/>
+        <th v-html="versionStr" v-if="showVersion"/>
+        <th v-html="jailbreakStr" v-if="showJailbreak"/>
+        <th v-html="releaseDateStr" v-if="showReleaseDate" style="width: 15%;"/>
       </tr>
-      <tr v-if="index == entryCount - 1 && !simpleTable"><td :colspan="(simpleTable) ? 3 : (showBuildNum + showVersion + showJailbreak + showReleaseDate)">{{loadingStr}}</td></tr>
-    </template>
-  </table>
+      <template v-for="(fw, index) in fwArr" :key="fw">
+        <tr v-if="simpleTable">
+          <td v-for="i in (reverseSorting) ? Object.keys(fw).reverse() : Object.keys(fw)" :key="i">{{ fw[i].version }}</td>
+          <td v-if="fw.startBuild.jailbreakArr.length"><router-link :to="jailbreakPath + fw.startBuild.jailbreakArr[0].name.replace(/ /g, '-') + '.html'">{{ fw.startBuild.jailbreakArr[0].name }}</router-link></td>
+          <td v-else v-html="noJbStr"/>
+        </tr>
+        <tr v-else>
+          <td v-if="showBuildNum"><a :href="firmwarePath + fw.uniqueBuild + '.html'">{{fw.build}}</a></td>
+
+          <template v-if="showVersion">
+            <td v-if="!showBuildNum"><a :href="firmwarePath + fw.uniqueBuild + '.html'">{{fw.osStr}} {{fw.version}} <span v-if="fwArr.filter(x => x.version == fw.version && x.osStr == fw.osStr).length > 1">({{fw.build}})</span></a></td>
+            <td v-else>{{fw.osStr}} {{fw.version}}</td>
+          </template>
+          
+          <template v-if="showJailbreak">
+            <td v-if="fw.jailbreakArr.length > 0"><span v-for="(jb, index) in fw.jailbreakArr" :key="jb"><a :href="jailbreakPath + jb.name.replace(/ /g, '-') + '.html'" v-html="jb.name"/><span v-if="index+1 < fw.jailbreakArr.length">, </span></span></td>
+            <td v-else v-html="noJbStr"/>
+          </template>
+          
+          <td v-if="showReleaseDate">{{fw.released}}</td>
+        </tr>
+        <tr v-if="index == entryCount - 1 && !simpleTable"><td :colspan="(simpleTable) ? 3 : (showBuildNum + showVersion + showJailbreak + showReleaseDate)">{{loadingStr}}</td></tr>
+      </template>
+    </table>
+  </div>
 </template>
 
 <script>
