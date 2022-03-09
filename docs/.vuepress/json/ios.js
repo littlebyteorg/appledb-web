e2cc29ba23b9c82e33913baf64820dad31d13b82
emiyl <me@emiyl.com>
2022-03-09 23:40:46 +0000

    move sorting over to js at compile

--- docs/.vuepress/json/ios.js
+++ docs/.vuepress/json/ios.js
@@ -59,4 +59,83 @@ iosArr = iosArr.map(function(x) {
   return x
 })
 
+
+
+function versionCompare(v1, v2, options) {
+  var lexicographical = options && options.lexicographical,
+      zeroExtend = options && options.zeroExtend,
+      v1parts = v1.split('.'),
+      v2parts = v2.split('.')
+
+  function isValidPart(x) {
+    return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x)
+  }
+
+  if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) return NaN
+
+  if (zeroExtend) {
+    while (v1parts.length < v2parts.length) v1parts.push("0");
+    while (v2parts.length < v1parts.length) v2parts.push("0");
+  }
+
+  if (!lexicographical) {
+    v1parts = v1parts.map(Number);
+    v2parts = v2parts.map(Number);
+  }
+
+  for (var i = 0; i < v1parts.length; ++i) {
+    if (v2parts.length == i) return 1
+    if (v1parts[i] == v2parts[i]) continue
+    else if (v1parts[i] > v2parts[i]) return 1
+    else return -1
+  }
+
+  if (v1parts.length != v2parts.length) return -1
+  return 0;
+}
+
+iosArr = iosArr.sort(function(a,b) {
+  var v = [a.version, b.version]
+  if (a.sortVersion) v[0] = a.sortVersion
+  if (b.sortVersion) v[1] = b.sortVersion
+  function getVerStr(x) { return x.split(' ')[0] }
+  var compVerStr = versionCompare(getVerStr(v[0]), getVerStr(v[1]))
+  if (compVerStr != 0) return compVerStr
+  else {
+    const verInclGM   = [v[0].includes('GM'), v[1].includes('GM')]
+    const verInclBeta = [v[0].includes('beta'), v[1].includes('beta')]
+    const verInclRC   = [v[0].includes('RC'), v[1].includes('RC')]
+    const beta        = [a.beta, b.beta]
+    
+    if (beta[1] - beta[0] != 0) return beta[1] - beta[0]
+    if (verInclRC[0] - verInclRC[1] != 0) return verInclRC[0] - verInclRC[1]
+    if (verInclGM[0] - verInclGM[1] != 0) return verInclGM[0] - verInclGM[1]
+    if (verInclBeta[0] - verInclBeta[1] != 0) return verInclBeta[0] - verInclBeta[1]
+
+    if (a.beta && b.beta) {
+      const betaNum = [v[0], v[1]]
+        .map(x => x.split(' '))
+        .map(x => x[2])
+        .map(x => (x == undefined) ? '1' : x)
+        .map(x => (x == '6-enterprise') ? '7' : x)
+        .map(x => parseFloat(x))
+      if (betaNum[0] - betaNum[1] != 0) return betaNum[0] - betaNum[1]
+    }
+  }
+  var dates = new Date(a.released).valueOf() - new Date(b.released).valueOf()
+  if (dates != 0) return dates
+  if (a.osStr > b.osStr) return -1
+  if (a.osStr < b.osStr) return 1
+  if (a.build < b.build) return -1
+  if (a.build > b.build) return 1
+  return 0
+})
+
+iosArr = iosArr.map(function(x) {
+  x.istvOS = (x.osStr == 'tvOS' || x.osStr == 'Apple TV Software')
+  x.isiOS = (x.osStr == 'iOS' || x.osStr == 'iPadOS' || x.osStr == 'iPhoneOS')
+  x.iswatchOS = (x.osStr == 'watchOS')
+  return x
+})
+
 module.exports = iosArr;
