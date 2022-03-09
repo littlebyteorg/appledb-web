c82678eaf71ea3addbefa3ee6279f87cc72a9401
emiyl <me@emiyl.com>
2022-03-09 20:36:25 +0000

    fix sorting

--- docs/.vuepress/components/device.vue
+++ docs/.vuepress/components/device.vue
@@ -423,8 +423,11 @@ export default {
               .map(x => x[2])
               .map(x => (x == undefined) ? '1' : x)
               .map(x => (x == '6-enterprise') ? '7' : x)
+              .map(x => parseFloat(x))
+            if (betaNum[0] - betaNum[1] != 0) return betaNum[0] - betaNum[1]
+            /*  .map(x => x + x.split('.').length < 1 ? '.0' : '')
             compVerStr = versionCompare(v[0], v[1])
-            if (compVerStr != 0) return compVerStr
+            if (compVerStr != 0) return compVerStr*/
           }
 
           return a.released - b.released
