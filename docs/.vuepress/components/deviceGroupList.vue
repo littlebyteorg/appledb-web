facc65124b920bf2e070524e3e29395e26cc5467
emiyl <me@emiyl.com>
2022-04-06 00:41:16 +0100

    add airpods to home and accessories

--- docs/.vuepress/components/deviceGroupList.vue
+++ docs/.vuepress/components/deviceGroupList.vue
@@ -87,6 +87,7 @@ export default {
                         "Apple Watch",
                         "Apple TV",
                         "HomePod",
+                        "AirPods",
                         "AirTag"
                     ]
                 }
