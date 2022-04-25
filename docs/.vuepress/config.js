e7ea7431a086ae19a36ee67d7be403296ed34816
emiyl <me@emiyl.com>
2022-04-25 04:27:00 +0100

    added navbar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,6 +10,24 @@ module.exports = {
       selectLanguageText: '<i class="fas fa-globe"/>',
       logo: '/assets/images/logo.png',
       logoDark: '/assets/images/logo_dark.png',
+      navbar: [
+        {
+          text: 'Devices',
+          link: '/device-selection.html'
+        },
+        {
+          text: 'Firmwares',
+          link: '/firmwares.html'
+        },
+        {
+          text: 'Credits',
+          link: '/credits.html'
+        },
+        {
+          text: 'iOS Guide',
+          link: 'https://ios.cfw.guide/'
+        }
+      ]
     },
 
     plugins: [
