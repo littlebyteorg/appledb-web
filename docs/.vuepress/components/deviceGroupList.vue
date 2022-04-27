cb2d03250b9ed55925291048b712a8211a8eea67
emiyl <me@emiyl.com>
2022-04-27 12:59:44 +0100

    add kb to accessories

--- docs/.vuepress/components/deviceGroupList.vue
+++ docs/.vuepress/components/deviceGroupList.vue
@@ -89,6 +89,7 @@ export default {
                         "Apple Pencil",
                         "AirTag",
                         "Remote",
+                        "Keyboard",
                         "Power"
                     ]
                 },
