eba203d8da99ef16e83c663bacf60298caf1aa2f
emiyl <me@emiyl.com>
2022-05-02 19:07:32 +0100

    js fix

--- docs/.vuepress/components/device.vue
+++ docs/.vuepress/components/device.vue
@@ -454,7 +454,7 @@ export default {
             this.options.filterDev = this.fm.deviceFilter[0].value
         }
 
-        if (fm.extraInfo && fm.extraInfo.length > 1) this.activeTab = this.fm.extraInfo[0].type
+        if (this.fm.extraInfo && this.fm.extraInfo.length > 1) this.activeTab = this.fm.extraInfo[0].type
 
         this.maxImgCount = this.fm.imgCount
     },
