b27a1799811af95cd9517975d1172bab1a34d1cc
emiyl <me@emiyl.com>
2022-08-10 19:41:00 +0100

    fixed blockquote

--- generatePageData/generatePageStubs.js
+++ generatePageData/generatePageStubs.js
@@ -7,7 +7,7 @@ function mkdirSync(dir) { if (!fs.existsSync(dir)) { fs.mkdirSync(dir) } }
 
 mkdirSync('./docs/.vuepress/dist/firmware')
 Array.from(new Set(osArr.map(x => x.osStr))).map(x => mkdirSync(`./docs/.vuepress/dist/firmware/${x}`))
-const page = fs.readFileSync('./docs/.vuepress/dist/404.html', 'utf8').replace('Looks like we&#39;ve got some broken links.','Retrieving data...')
+const page = fs.readFileSync('./docs/.vuepress/dist/404.html', 'utf8')
 
 for (const os of osArr) {
     let parsedPage = parse(page)
@@ -15,6 +15,7 @@ for (const os of osArr) {
     parsedPage.querySelectorAll('meta').find(x => x.rawAttrs.includes('name="description"')).rawAttrs = `name="description" content="Information for ${os.osStr} version ${os.version}"`
     parsedPage.querySelector('title').innerHTML = `${os.osStr} ${os.version} ${(os.build != os.version) ? `(${os.build})` : ''}`
     parsedPage.querySelector('h1').innerHTML = `${os.osStr} ${os.version} ${(os.build != os.version) ? `(${os.build})` : ''}`
+    parsedPage.querySelector('blockquote').innerHTML = 'Retrieving data...'
 
     fs.writeFile(`./docs/.vuepress/dist/firmware/${os.osStr}/${os.uniqueBuild}.html`, parsedPage.toString(), (err) => {
         if (err) console.log(err)
