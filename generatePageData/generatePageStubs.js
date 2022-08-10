4d5848c24520bc0034c06810bedc0e732c2d00ce
emiyl <me@emiyl.com>
2022-08-10 19:47:37 +0100

    improved css on redirect pages

--- generatePageData/generatePageStubs.js
+++ generatePageData/generatePageStubs.js
@@ -8,6 +8,9 @@ function mkdirSync(dir) { if (!fs.existsSync(dir)) { fs.mkdirSync(dir) } }
 mkdirSync('./docs/.vuepress/dist/firmware')
 Array.from(new Set(osArr.map(x => x.osStr))).map(x => mkdirSync(`./docs/.vuepress/dist/firmware/${x}`))
 const page = fs.readFileSync('./docs/.vuepress/dist/404.html', 'utf8')
+.replace('<div class="theme-container">','<div class="theme-container no-sidebar">')
+.replace('<div class="not-found-section theme-default-content">','<main class="page"><div class="theme-default-content">')
+.replace('Take me home</a></div>','Take me home</a></div></main>')
 
 for (const os of osArr) {
     let parsedPage = parse(page)
