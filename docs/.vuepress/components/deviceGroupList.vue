2e9e63cfbf087cbf77d93c6f0a38255fa4d3fcf9
emiyl <me@emiyl.com>
2023-09-04 13:55:13 +0100

    improve software selection

--- docs/.vuepress/components/deviceGroupList.vue
+++ docs/.vuepress/components/deviceGroupList.vue
@@ -1,67 +1,247 @@
 <template>
-    <div @click="unexpandMenu()">
-        <p v-for="s in introStr" :key="s">{{ s }}</p>
-    </div>
-
-    <div class="groupLabelWrapper">
-        <div class="flexWrapper" @click="unexpandMenu()">
-            <div
-                v-for="o in groupObj" :key="o"
-                :class="[
-                    'groupLabel',
-                    activeGroup == o.id ? 'active' : ''
-                ]"
-                @click="(activeGroup = o.id)"
-                :ref="`groupLabel-${o.id}`"
-            >
-                {{ o.label }}
-            </div>
+    <div>
+        <div @click="unexpandMenu()">
+            <p v-for="s in introStr" :key="s">{{ s }}</p>
         </div>
-        <div class="flexWrapper">
-            <label
-                for="checkbox"
-                @click="getWrappedLabels() && (expandedMenu = !expandedMenu)"
-            >
-                <div class="groupLabel icon">
-                        <i v-if="expandedMenu" class="fas fa-times"></i>
-                        <i v-else class="fas fa-bars"></i>
+
+        <div class="groupLabelWrapper">
+            <div class="flexWrapper" @click="unexpandMenu()">
+                <div
+                    v-for="o in groupObj" :key="o"
+                    :class="[
+                        'groupLabel',
+                        activeGroup == o.id ? 'active' : ''
+                    ]"
+                    @click="(activeGroup = o.id)"
+                    :ref="`groupLabel-${o.id}`"
+                >
+                    {{ o.label }}
                 </div>
-            </label>
-        </div>
-    </div>
-
-    <input id="checkbox" type="checkbox" name="menu" ref="checkbox"/>
-    <ul class="submenu">
-        <li
-            v-for="group in wrappedGroups" :key="group"
-            :class="[
-                activeGroup == group.id ? 'active' : ''
-            ]"
-            @click="activeGroup = group.id"
-        >
-            {{ group.label }}
-        </li>
-    </ul>
-
-    <div @click="unexpandMenu()">
-        <template v-for="o in groupObj.filter(x => x.id == activeGroup)" :key="o">
-            <div class="gridWrapper">
-                <div v-for="t in o.types" :key="t" class="gridItem"><router-link :to="`/device-selection/${t.fdn()}.html`">
-                    <div class="imgWrapper"><picture>
-                        <source :srcset="imageObj[t] + '.avif'" type="image/avif">
-                        <source :srcset="imageObj[t] + '.webp'" type="image/webp">
-                        <img :src="imageObj[t] + '.png'">
-                    </picture></div>
-                    <div class="title">{{t}}</div>
-                </router-link></div>
             </div>
-        </template>
+            <div class="flexWrapper">
+                <label
+                    for="checkbox"
+                    @click="getWrappedLabels() && (expandedMenu = !expandedMenu)"
+                >
+                    <div class="groupLabel icon">
+                            <i v-if="expandedMenu" class="fas fa-times"></i>
+                            <i v-else class="fas fa-bars"></i>
+                    </div>
+                </label>
+            </div>
+        </div>
 
-        <p><router-link to="/device-list.html">{{ viewAllStr }}</router-link></p>
+        <input id="checkbox" type="checkbox" name="menu" ref="checkbox"/>
+        <ul class="submenu">
+            <li
+                v-for="group in wrappedGroups" :key="group"
+                :class="[
+                    activeGroup == group.id ? 'active' : ''
+                ]"
+                @click="activeGroup = group.id"
+            >
+                {{ group.label }}
+            </li>
+        </ul>
 
-        <p>AppleDB is not affiliated with Apple Inc.</p>
+        <div @click="unexpandMenu()">
+            <template v-for="o in groupObj.filter(x => x.id == activeGroup)" :key="o">
+                <div class="gridWrapper">
+                    <div v-for="t in o.types" :key="t" class="gridItem">
+                        <router-link :to="o.label == 'Software' ?
+                            `/device/${t.fdn()}.html` :
+                            `/device-selection/${t.fdn()}.html`
+                        ">
+                            <div class="imgWrapper"><picture>
+                                <source :srcset="imageObj[t] + '.avif'" type="image/avif">
+                                <source :srcset="imageObj[t] + '.webp'" type="image/webp">
+                                <img :src="imageObj[t] + '.png'">
+                            </picture></div>
+                            <div class="title">{{t}}</div>
+                        </router-link>
+                    </div>
+                </div>
+            </template>
+
+            <p><router-link to="/device-list.html">{{ viewAllStr }}</router-link></p>
+
+            <p>AppleDB is not affiliated with Apple Inc.</p>
+        </div>
     </div>
 </template>
+
+<script>
+import { usePageFrontmatter } from '@vuepress/client'
+import { useDarkMode } from '@vuepress/theme-default/lib/client/composables'
+import deviceTypeGroups from '@temp/deviceTypeGroups'
+import substitutes from '../plugins/writeTemp/lib/deviceNameSubstitutes.json'
+
+String.prototype.fdn = function() {
+    let arr = this.split('')
+    for (let c in arr) for (var i = 0; i < substitutes.length / 2; i++)
+        if (arr[c] == substitutes[i*2]) arr[c] = substitutes[i*2+1]
+    return arr.join('')
+}
+
+export default {
+    data() {
+        return {
+            introStr: [
+                'Please select what kind of device you have below.'
+            ],
+            viewAllStr: 'View all devices',
+            activeGroup: 0,
+            wrappedGroups: [],
+            expandedMenu: false,
+            hasSoftwareType: false,
+
+            frontmatter: usePageFrontmatter(),
+            isDarkMode: useDarkMode()
+        }
+    },
+    computed: {
+        groupList() {
+            return this.frontmatter.groupList
+        },
+        typeArr() {
+            let ret = Array.from(new Set(this.groupList.map(x => x.type)))
+            if (ret.includes('Software')) {
+                this.hasSoftwareType = true
+                ret = ret.filter(x => x != 'Software')
+            }
+            return ret
+        },
+        groupObj() {
+            var presetTypeArr = []
+            var presetOrder = deviceTypeGroups.map(x => {
+                x.types = x.types.filter(y => this.typeArr.includes(y))
+                for (const t of x.types) presetTypeArr.push(t)
+                return x
+            })
+
+            if (this.hasSoftwareType) presetOrder.push({
+                label: "Software",
+                types: this.groupList.filter(x => x.type == 'Software').map(x => x.groupKey)
+            })
+
+            const unsetTypes = this.typeArr.filter(x => !presetTypeArr.includes(x))
+            if (unsetTypes.length > 0) presetOrder.push({
+                label: "Other",
+                types: unsetTypes
+            })
+
+            for (var i = 0; i < presetOrder.length; i++) {
+                presetOrder[i].id = i
+            }
+
+            return presetOrder
+        },
+        devCount() {
+            const groupList = this.groupList
+            var tempTypeArr = []
+            var ret = {}
+
+            for (const g of groupList) {
+                if (tempTypeArr.includes(g.type)) continue
+                tempTypeArr.push(g.type)
+                ret[g.type] = 0
+            }
+
+            for (const g of groupList) ret[g.type] += g.devices.length
+            
+            return ret
+        },
+        imageObj() {
+            const groupList = this.groupList
+            var tempTypeArr = []
+            var firstDeviceObj = {}
+
+            const typeArr = Array.from(new Set(groupList.map(x => x.type)))
+
+            for (const g of groupList) {
+                if (tempTypeArr.includes(g.type) || g.img.count < 1) continue
+                tempTypeArr.push(g.type)
+                firstDeviceObj[g.type] = {
+                    key: g.devices[0],
+                    imageBool: g.img.count > 0,
+                    dark: g.img.dark
+                }
+            }
+
+            const missingTypes = typeArr.filter(x => !Object.keys(firstDeviceObj).includes(x))
+            for (const type of missingTypes) {
+                const firstGroup = groupList.filter(x => x.type == type)[0]
+                firstDeviceObj[type] = {
+                    key: firstGroup.groupKey,
+                    imageBool: firstGroup.img.count > 0,
+                    dark: firstGroup.img.dark
+                }
+            }
+
+            if (this.hasSoftwareType) {
+                for (const software of this.groupList.filter(x => x.type == "Software")) {
+                    firstDeviceObj[software.name] = {
+                        key: software.devices[0],
+                        imageBool: software.img.count > 0,
+                        dark: software.img.dark
+                    }
+                }
+            }
+
+            const overrides = { 
+                AirPods: "AirPods1,1",
+            }
+            
+            for (const o in overrides) {
+                const group = groupList.filter(x => {
+                    if (!x.key) return false
+                    return x.key.includes(overrides[o])
+                })[0]
+                if (!group) continue
+                firstDeviceObj[o] = {
+                    key: group.devices[0],
+                    imageBool: group.img.count > 0,
+                    dark: group.img.dark
+                }
+            }
+
+            var ret = {}
+            for (const d in firstDeviceObj) ret[d] = firstDeviceObj[d].imageBool ?
+                `https://img.appledb.dev/device@preview/${firstDeviceObj[d].key}/0${this.isDarkMode && firstDeviceObj[d].dark ? '_dark' : ''}` :
+                `https://img.appledb.dev/device@preview/logo/0${this.isDarkMode ? '_dark' : ''}`
+            
+            return ret
+        }
+    },
+    methods: {
+        async getWrappedLabels() {
+            let id = 0
+            let previousTop = -1
+            for (var i of this.groupObj) {
+                const [groupLabel] = this.$refs[`groupLabel-${i.id}`]
+                let top = groupLabel.getBoundingClientRect().top
+                
+                if (previousTop < 0 || previousTop == top) {
+                    previousTop = top
+                    id = i.id
+                    continue
+                } else break
+            }
+            
+            this.wrappedGroups = this.groupObj.slice(id+1)
+        },
+        unexpandMenu() {
+            this.expandedMenu = false
+            this.$refs.checkbox.checked = false
+        }
+    },
+    mounted() {
+        window.onresize = () => {
+            this.getWrappedLabels();
+        }
+    }
+}
+</script>
 
 <style scoped lang="scss">
 input[type=checkbox] {
@@ -209,155 +389,3 @@ input[type=checkbox]:checked~.submenu {
     }
 }
 </style>
-
-<script>
-import { usePageFrontmatter } from '@vuepress/client'
-import { useDarkMode } from '@vuepress/theme-default/lib/client/composables'
-import deviceTypeGroups from '@temp/deviceTypeGroups'
-import substitutes from '../plugins/writeTemp/lib/deviceNameSubstitutes.json'
-
-String.prototype.fdn = function() {
-    let arr = this.split('')
-    for (let c in arr) for (var i = 0; i < substitutes.length / 2; i++)
-        if (arr[c] == substitutes[i*2]) arr[c] = substitutes[i*2+1]
-    return arr.join('')
-}
-
-export default {
-    data() {
-        return {
-            introStr: [
-                'Please select what kind of device you have below.'
-            ],
-            viewAllStr: 'View all devices',
-            activeGroup: 0,
-            wrappedGroups: [],
-            expandedMenu: false,
-
-            frontmatter: usePageFrontmatter(),
-            isDarkMode: useDarkMode()
-        }
-    },
-    computed: {
-        groupList() {
-            return this.frontmatter.groupList
-        },
-        typeArr() {
-            return Array.from(new Set(this.groupList.map(x => x.type)))
-        },
-        groupObj() {
-            var presetTypeArr = []
-            var presetOrder = deviceTypeGroups.map(x => {
-                x.types = x.types.filter(y => this.typeArr.includes(y))
-                for (const t of x.types) presetTypeArr.push(t)
-                return x
-            })
-
-            const unsetTypes = this.typeArr.filter(x => !presetTypeArr.includes(x))
-            if (unsetTypes.length > 0) presetOrder.push({
-                label: "Other",
-                types: unsetTypes
-            })
-
-            for (var i = 0; i < presetOrder.length; i++) {
-                presetOrder[i].id = i
-            }
-
-            return presetOrder
-        },
-        devCount() {
-            const groupList = this.groupList
-            var tempTypeArr = []
-            var ret = {}
-
-            for (const g of groupList) {
-                if (tempTypeArr.includes(g.type)) continue
-                tempTypeArr.push(g.type)
-                ret[g.type] = 0
-            }
-
-            for (const g of groupList) ret[g.type] += g.devices.length
-            
-            return ret
-        },
-        imageObj() {
-            const groupList = this.groupList
-            var tempTypeArr = []
-            var firstDeviceObj = {}
-
-            const typeArr = Array.from(new Set(groupList.map(x => x.type)))
-
-            for (const g of groupList) {
-                if (tempTypeArr.includes(g.type) || g.img.count < 1) continue
-                tempTypeArr.push(g.type)
-                firstDeviceObj[g.type] = {
-                    key: g.devices[0],
-                    imageBool: g.img.count > 0,
-                    dark: g.img.dark
-                }
-            }
-
-            const missingTypes = typeArr.filter(x => !Object.keys(firstDeviceObj).includes(x))
-            for (const type of missingTypes) {
-                const firstGroup = groupList.filter(x => x.type == type)[0]
-                firstDeviceObj[type] = {
-                    key: firstGroup.devices[0],
-                    imageBool: firstGroup.img.count > 0,
-                    dark: firstGroup.img.dark
-                }
-            }
-
-            const overrides = { 
-                AirPods: "AirPods1,1",
-            }
-            
-            for (const o in overrides) {
-                const group = groupList.filter(x => {
-                    if (!x.key) return false
-                    return x.key.includes(overrides[o])
-                })[0]
-                if (!group) continue
-                firstDeviceObj[o] = {
-                    key: group.devices[0],
-                    imageBool: group.img.count > 0,
-                    dark: group.img.dark
-                }
-            }
-
-            var ret = {}
-            for (const d in firstDeviceObj) ret[d] = firstDeviceObj[d].imageBool ?
-                `https://img.appledb.dev/device@preview/${firstDeviceObj[d].key}/0${this.isDarkMode && firstDeviceObj[d].dark ? '_dark' : ''}` :
-                `https://img.appledb.dev/device@preview/logo/0${this.isDarkMode ? '_dark' : ''}`
-            
-            return ret
-        }
-    },
-    methods: {
-        async getWrappedLabels() {
-            let id = 0
-            let previousTop = -1
-            for (var i of this.groupObj) {
-                const [groupLabel] = this.$refs[`groupLabel-${i.id}`]
-                let top = groupLabel.getBoundingClientRect().top
-                
-                if (previousTop < 0 || previousTop == top) {
-                    previousTop = top
-                    id = i.id
-                    continue
-                } else break
-            }
-            
-            this.wrappedGroups = this.groupObj.slice(id+1)
-        },
-        unexpandMenu() {
-            this.expandedMenu = false
-            this.$refs.checkbox.checked = false
-        }
-    },
-    mounted() {
-        window.onresize = () => {
-            this.getWrappedLabels();
-        }
-    }
-}
-</script>
